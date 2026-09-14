968b6b25da596a3d44561bd3bb4ca52f151a6d57
idrissi Mohammed <idmohame89@gmail.com>
2026-09-14 12:01:57 +0100

    implement arcjet security...

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { wsArcjet } from "../arcjet";
 
 function sendJson(socket, payload) {
     if(socket.readyState !== WebSocket.OPEN){
@@ -9,9 +10,8 @@ function sendJson(socket, payload) {
 
 function broadcast(wss, payload){
     for(const client of wss.clients){
-        if(socket.readyState !== WebSocket.OPEN){
-        return;
-    }
+        if(socket.readyState !== WebSocket.OPEN)continue;
+
     client.send(JSON.stringify(payload));
 }
 }
@@ -19,10 +19,44 @@ function broadcast(wss, payload){
 export function attachWebSocketServer(server){
     const wss = new WebSocketServer({server, path:'/ws', maxPayload:1024 * 1024,});
 
-    wss.on('connection', (socket) => {
+    wss.on('connection', async(socket, req) => {
+        if(wsArcjet) {
+            try {
+                const decision = await wsArcjet.protect(req);
+
+                if(decision.isDenied()) {
+                    const code = decision.reason.isRateLimit() ? 1013 : 1008;
+                    const reason = decision.reason.isRateLimit() ? 'Rate limit exceeded' : 'Access denied';
+
+                    socket.close(code.reason);
+                    return;
+                }
+
+            } catch (e) {
+                console.error('WS connection error', e);
+                socket.close(1011, 'Server security error');
+                return;
+            }
+        }
+
+        socket.isAlive = true;
+        socket.on('pong', () => { socket.isAlive = true});
+
         sendJson(socket, { type: 'welcome'});
+        
         socket.on('error', console.error);
-    })
+    });
+
+    const interval = setInterval(() => {
+        wss.clients.forEach((ws) => {
+            if(ws.isAlive === false) return ws.terminate();
+            ws.isAlive = false;
+            ws.ping();
+             });
+        }, 30000);
+
+        wss.on('close', () => clearInterval(interval));
+   
 
     function broadcastMatchCreated(match) {
         broadcast(wss, { type: 'match_created', data: match});
